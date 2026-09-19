b7c832c6132795567feef1eb76537a5de289a1c4
sdnjatiwaringin13-boop <sdnjatiwaringin.13@gmail.com>
2026-09-19 14:05:39 +0700

    Update API_URL to the correct Google Apps Script URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "MASUKKAN_URL_APPS_SCRIPT_DI_SINI";
+  "https://script.google.com/macros/s/AKfycbw6WR2c4zx59S84HRruF5vtJJXAla1KjYGN-tk4RDBRt1MQK4IUNCna9PYzTNzNst9u/exec";
 
 
 const loginForm =
